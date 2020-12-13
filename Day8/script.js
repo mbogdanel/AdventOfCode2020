9622539aa1af58d16d651e72422483e134cc7b2a
Marius Bogdanel <marius.bogdanel@gmail.com>
2020-12-13 21:24:56 +0000

    day8 part1

--- Day8/script.js
+++ Day8/script.js
@@ -7,9 +7,36 @@ fs.readFile('./input.txt', (err, data) => {
   }
   const stringInput = data.toString()
 
-  const arrayInput = stringInput.split('\r\n')
+  const arrayInput = stringInput.split('\n')
 
-  console.log(arrayInput)
+  stringToObject = (str) => {
+    const obj = {}
+    const [k, v] = str.split(' ')
+    obj[k] = parseInt(v)
+    return obj
+  }
 
+  let listInstructions = arrayInput.map(stringToObject)
+
+  let i = 0
+  let acc = 0
+
+  while (true) {
+    if ('beenHere' in listInstructions[i]) {
+      break
+    }
+    if ('acc' in listInstructions[i]) {
+      acc += listInstructions[i].acc
+      listInstructions[i].beenHere = true
+      i++
+    } else if ('jmp' in listInstructions[i]) {
+      listInstructions[i].beenHere = true
+      i += listInstructions[i].jmp
+    } else if ('nop' in listInstructions[i]) {
+      listInstructions[i].beenHere = true
+      i++
+    }
+  }
+  console.log(acc)
   console.timeEnd('adventOfCodeDay2')
 })
