807d113bb69aea7efdd8b84393375b5730d8f726
Marius Bogdanel <marius.bogdanel@gmail.com>
2020-12-20 09:20:38 +0000

    day6 solutions

--- Day6/script.js
+++ Day6/script.js
@@ -9,11 +9,7 @@ fs.readFile('./input.txt', (err, data) => {
 
   let arr = stringInput.split('\r\n\r\n')
 
-  // problem is here, need array with concat string.............................
   const newArray = arr.map((part) => part.split('\r\n').join(''))
-
-  // console.log(newArray)
-  console.log(arr)
 
   function unique_char(string) {
     var unique = ''
@@ -30,7 +26,24 @@ fs.readFile('./input.txt', (err, data) => {
     count += unique_char(newArray[i]).length
   }
 
-  console.log(count)
+  ///////// Part2
+  let count2 = 0
+  let tempContainer = []
+  for (i = 0; i < arr.length; i++) {
+    tempContainer = []
+    let numberPeople = arr[i].split(/\r\n|\r|\n/).length
+    for (j = 0; j < newArray[i].length; j++) {
+      if (
+        !tempContainer.includes(newArray[i].charAt(j)) &&
+        newArray[i].split(newArray[i].charAt(j)).length - 1 === numberPeople
+      ) {
+        tempContainer.push(newArray[i].charAt(j))
+        count2++
+      }
+    }
+  }
 
+  console.log(count)
+  console.log(count2)
   console.timeEnd('adventOfCodeDay2')
 })
